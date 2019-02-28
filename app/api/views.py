b79c4edcde72272b2c96694466fc28ea4c759513
Victor Ughonu <victorughonu95@gmail.com>
2019-02-28 13:43:19 +0100

    modified the schemas to accommodate nesting and added new api endpoints

--- app/api/views.py
+++ app/api/views.py
@@ -2,7 +2,8 @@ from flask import render_template, request, jsonify
 from app import db
 from app.models import (Location, Update, State, Lga)
 from app.schema import (state_schema, states_schema, lga_schema, lgas_schema,
-                        location_schema, locations_schema, update_schema, updates_schema, recent_schema)
+                        location_schema, locations_schema, update_schema, updates_schema, recent_schema, location_updates_schema, lga_locations_schema,
+                        lga_location_updates_schema)
 
 
 from . import bp
@@ -103,6 +104,58 @@ def delete_update(id):
 
 @bp.route('/api/recent/<string:location_slug>', methods=["GET"])
 def get_recent(location_slug):
-    location = Location.query.filter(Location.slug_name == location_slug).first()
+    location = Location.query.filter(
+        Location.slug_name == location_slug).first()
     update = location.updates[-1]
     return jsonify(recent_schema.dump(update).data)
+
+
+@bp.route('/api/lga-locations', methods=['GET'])
+def lga_with_locations():
+    lga_locations = Lga.query.all()
+
+    return jsonify(lga_locations_schema.dump(lga_locations).data)
+
+
+@bp.route('/api/location-updates', methods=['GET'])
+def location_with_updates():
+    location_updates = Location.query.all()
+    return jsonify(location_updates_schema.dump(location_updates).data)
+
+
+@bp.route('/api/lga-location-updates', methods=['GET'])
+def lga_location_update():
+    lga_location_updates = Lga.query.all()
+    return jsonify(lga_location_updates_schema.dump(lga_location_updates).data)
+
+
+@bp.route('/api/average-price', methods=['GET'])
+def average_price():
+    locations = Location.query.all()
+    last_update_per_location = (
+        location.updates[-1] for location in locations if len(location.updates) > 0)
+    prices = ((update.PMS, update.AGO, update.DPK)
+              for update in last_update_per_location)
+    PMS = 0
+    AGO = 0
+    DPK = 0
+    n = 0
+    for price in prices:
+        PMS = PMS + price[0]
+        AGO = AGO + price[1]
+        DPK = DPK + price[2]
+        n = n + 1
+    average = {}
+    if n != 0:
+        average = {'PMS': round(PMS / n, 2),
+                   'AGO': round(AGO / n), 'DPK': round(DPK / n, 2)}
+
+    return jsonify(average)
+
+
+@bp.route('/api/location-recent', methods=['GET'])
+def location_recent_update():
+    locations = Location.query.all()
+    recents = (l.updates[-1] for l in locations if len(l.updates) > 0)
+
+    return jsonify(updates_schema.dump(recents).data)
--- app/schema.py
+++ app/schema.py
@@ -2,54 +2,46 @@ from app import ma, db
 from app.models import State, Lga, Location, Update
 
 
-class StateSchema(ma.ModelSchema):
-    class Meta:
-        model = State
-
-    # Smart hyperlinking
-    links = ma.Hyperlinks({
-        'self': ma.URLFor('api.state_detail', id='<id>'),
-        'collection': ma.URLFor('api.get_states')
-    })
-
-
-states_schema = StateSchema(many=True)
-state_schema = StateSchema()
-
-
 class LgaSchema(ma.ModelSchema):
     class Meta:
         model = Lga
-    links = ma.Hyperlinks({
-        'self': ma.URLFor('api.lga_detail', id='<id>'),
-        'collection': ma.URLFor('api.get_lgas')
-    })
+    locations = ma.Nested('LocationSchema', many=True,
+                          only=('id', 'slug_name', 'name'))
 
 
-lgas_schema = LgaSchema(many=True)
+lgas_schema = LgaSchema(many=True, exclude=('locations', ))
 lga_schema = LgaSchema()
+
+
+class StateSchema(ma.ModelSchema):
+    class Meta:
+        model = State
+    lgas = ma.Nested('LgaSchema', many=True, exclude=('state_id', 'locations'))
+
+
+states_schema = StateSchema(many=True, exclude=('lgas',))
+state_schema = StateSchema()
 
 
 class LocationSchema(ma.ModelSchema):
     class Meta:
         model = Location
-        fields = ('id', 'name', 'lga_id', 'updates', 'slug_name')
-    # links = ma.Hyperlinks({
-    #     'self': ma.URLFor('api.lga_detail', id='<id>'),
-    #     'collection': ma.URLFor('api.get_lgas')
-    # })
+        fields = ('id', 'name', 'lga_id', 'slug_name', 'state', 'updates')
+    updates = ma.Nested('UpdateSchema', many=True)
+    state = ma.Nested('LgaSchema', only='lga_id.state.name')
 
 
-locations_schema = LocationSchema(many=True)
+locations_schema = LocationSchema(many=True, exclude=('updates', ))
 location_schema = LocationSchema()
 
 
 class UpdateSchema(ma.ModelSchema):
     class Meta:
         model = Update
+    location = ma.Nested('LocationSchema', only=('id', 'name'))
 
 
-updates_schema = UpdateSchema(many=True)
+updates_schema = UpdateSchema(many=True, exclude=('timestamp',))
 update_schema = UpdateSchema()
 
 
@@ -60,3 +52,42 @@ class RecentSchema(ma.ModelSchema):
 
 
 recent_schema = RecentSchema()
+
+
+class StateLgaSchema(ma.ModelSchema):
+    class Meta:
+        model = State
+    lgas = ma.Nested(LgaSchema, many=True)
+
+
+class LocationUpdateSchema(ma.ModelSchema):
+    class Meta:
+        model = Location
+    updates = ma.Nested(UpdateSchema, exclude=(
+        'timestamp', 'location_id'), many=True)
+    lga = ma.Nested('LgaSchema', only=('id', 'name'))
+
+
+location_updates_schema = LocationUpdateSchema(
+    many=True, exclude=('timestamp',))
+
+
+class LgaLocationSchema(ma.ModelSchema):
+    class Meta:
+        model = Lga
+    locations = ma.Nested(LocationSchema, many=True, exclude=('updates',))
+    state = ma.Nested('StateSchema', only=('id', 'name'))
+
+
+lga_locations_schema = LgaLocationSchema(many=True)
+
+
+class LgaLocationUpdateSchema(ma.ModelSchema):
+    class Meta:
+        model = Lga
+    locations = ma.Nested(LocationUpdateSchema,
+                          many=True, exclude=('timestamp',))
+    state = ma.Nested('StateSchema', only=('id', 'name'))
+
+
+lga_location_updates_schema = LgaLocationUpdateSchema(many=True)
